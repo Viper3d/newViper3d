514b21f6f8835a9186ff675faec6cdfe69133aed
Viper3d <150278499+Viper3d@users.noreply.github.com>
2024-06-10 19:02:08 +0200

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: process.env.PORT || 3000,  // Utiliza el puerto proporcionado por Railway o el puerto 3000 por defecto
+    host: true  // Asegura que Vite escuche en el host 0.0.0.0 para ser accesible externamente
+  }
 })
